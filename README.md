ed5262d7917472a117f449507e053f0aeb8571f6
Entityless <entityless@gmail.com>
2022-05-14 10:47:52 +0800

    [README][Refactor] Add README.md, and add comparison of speed and accuracy

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Optimization Coding Challenges
+
+Some optimization mini-tasks with validation.
+
+## 00 Bucket accumulator
+
+Given `buckets[bucket_count][bucket_size]` and `pools[pool_count][pool_size]`;
+
+For each element in `pool = pools[pool_id]`, for each `bucket_id` in range `bucket_count`, if `rand(0, 1) < fill_ratio`, we add it `fill_count` times to a random position in `buckets[bucket_id]`.
+
+Since we do the same operation for each pool, we can abstract above actions into an array of `tuple<bucket_id, pool_offset, bucket_offset>`, and perform:
+
+``` c++
+
+for (int pool_id = 0; pool_id < pool_count; pool_id++) {
+    for (auto t : accumulation_array) {
+        bucket_id = t.bucket_id;
+        pool_offset = t.pool_offset;
+        bucket_offset = t.bucket_offset;
+        pools[pool_id][pool_offset] = buckets[bucket_id][bucket_offset];
+    }
+}
+
+```
+
+Your are given `buckets` memset to 0, `pools` with random floating-point numbers, and theee arrays of `vector` format (SoA) representing the array of `tuple<bucket_id, pool_offset, bucket_offset>` (AoS). Since the operation of each pool is identical, you can do optimization on the reduction sequence using this feature. Also, the multi-thread hardware feature is also encouraged to be utilized.
--- tests/00_bucket_accumulator_cpu.cpp
+++ tests/00_bucket_accumulator_cpu.cpp
@@ -1,5 +1,3 @@
-
-
 #include <omp.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -24,14 +22,13 @@ size_t get_rand_int_in_range(size_t range) {
 }
 
 struct Buckets {
-    std::vector<std::vector<Element_type>> m_arrays;
+    std::vector<Element_type*> m_arrays;
 
     Buckets(size_t count, size_t size, bool random = false) {
         m_arrays.resize(count);
         for (size_t i = 0; i < count; i++) {
-            m_arrays[i].resize(size);
+            m_arrays[i] = (Element_type*) _mm_malloc(size * sizeof(Element_type), 4096);
 
-            // manual memset
             if (!random) {
                 for (size_t j = 0; j < size; j++) {
                     m_arrays[i][j] = 0;
@@ -93,7 +90,20 @@ void std_accumulation(Buckets& dst_buckets, const Buckets& src_pools, const Accu
             int pool_offset = seq.m_pool_offsets[i];
             int bucket_offset = seq.m_bucket_offsets[i];
 
-            dst_buckets.m_arrays.at(bucket_id).at(bucket_offset) += src_pools.m_arrays.at(pool_id).at(pool_offset);
+            dst_buckets.m_arrays[bucket_id][bucket_offset] += src_pools.m_arrays[pool_id][pool_offset];
+        }
+    }
+}
+
+void optimized_accumulation(Buckets& dst_buckets, const Buckets& src_pools, const Accumulation_sequence& seq) {
+    for (size_t pool_id = 0; pool_id < seq.m_pool_count; pool_id++) {
+        Element_type* pool = src_pools.m_arrays[pool_id];
+        for (size_t i = 0; i < seq.m_sequence_size; i++) {
+            int bucket_id = seq.m_bucket_ids[i];
+            int pool_offset = seq.m_pool_offsets[i];
+            int bucket_offset = seq.m_bucket_offsets[i];
+
+            dst_buckets.m_arrays[bucket_id][bucket_offset] += pool[pool_offset];
         }
     }
 }
@@ -108,18 +118,37 @@ int main(int argc, char** argv) {
     size_t pool_size = 64 * 1024 * 1024 / sizeof(Element_type);
 
     double fill_ratio = 0.2;
-    size_t fill_count = 10;
+    size_t fill_count = 4;
 
     Buckets std_buckets(bucket_count, bucket_size);
+    Buckets optimized_output_buckets(bucket_count, bucket_size);
     Buckets pools(pool_count, pool_size, true);
 
     Accumulation_sequence sequence(bucket_count, bucket_size, pool_count, pool_size, fill_ratio, fill_count);
 
-    double stt = omp_get_wtime();
-    std_accumulation(std_buckets, pools, sequence);
-    double edt = omp_get_wtime();
+    {
+        double stt = omp_get_wtime();
+        std_accumulation(std_buckets, pools, sequence);
+        double edt = omp_get_wtime();
+        printf("STD Elapsed = %f\n", edt - stt);
+    }
 
-    printf("Elapsed = %f\n", edt - stt);
+    {
+        double stt = omp_get_wtime();
+        optimized_accumulation(optimized_output_buckets, pools, sequence);
+        double edt = omp_get_wtime();
+        printf("OPT Elapsed = %f\n", edt - stt);
+    }
+
+    double diff_sum = 0;
+
+    for (int bucket_id = 0; bucket_id < bucket_count; bucket_id++) {
+        for (int bucket_offset = 0; bucket_offset < bucket_size; bucket_offset++) {
+            diff_sum += std::abs(std_buckets.m_arrays[bucket_id][bucket_offset] - optimized_output_buckets.m_arrays[bucket_id][bucket_offset]);
+        }
+    }
+
+    printf("diff sum = %f\n", diff_sum);
 
     return 0;
 }
